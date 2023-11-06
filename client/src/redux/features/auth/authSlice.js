68013e153a3faa835dea6610bdf18cc5da28006b
akshaychavhan <akshay@rtledgers.com>
2023-11-07 00:07:59 +0530

    13_redux setup completed

---
+++ client/src/redux/features/auth/authSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    loading: false,
+    user: null,
+    token: null,
+    error: null
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: initialState ,
+    reducer:{},
+    extraReducers:{}
+})
+
+
+export default authSlice;
