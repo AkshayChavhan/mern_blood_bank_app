63de616a352525558e19a6f0ea6f3f939b1b3886
akshaychavhan <akshay@rtledgers.com>
2023-11-06 16:45:30 +0530

    7 authentification middleware completed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { registerController, loginController } = require("../controllers/authController");
+const { registerController, loginController , currentUserController } = require("../controllers/authController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 
 
@@ -12,6 +13,9 @@ router.post('/register', registerController)
 
 // Login || POST
 router.post('/login', loginController);
+
+// GET CURRENT USER || GET
+router.get('/current-user', authMiddleware , currentUserController);
 
 
 module.exports = router ;
