8e57c89c08f3ae6f2de91dd255fb77f09e9afe52
akshaychavhan <akshay@rtledgers.com>
2023-11-06 19:15:30 +0530

    9 get inventory api completed

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,4 +1,4 @@
-const { createInventoryController } = require("../controllers/inventoryController");
+const { createInventoryController , getInventoryController } = require("../controllers/inventoryController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 const express = require("express");
@@ -10,6 +10,10 @@ const router = express.Router();
 // routes
 // ADD INVENTORY || POST
 router.post("/create-inventory", authMiddleware, createInventoryController)
+
+
+// GET ALL BLOOD RECORDS
+router.get("/get-inventory", authMiddleware, getInventoryController)
 
 
 module.exports = router ;
