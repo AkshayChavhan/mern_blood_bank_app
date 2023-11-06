45b281ba510e310749f606f4312c0b312dd20ce2
akshaychavhan <akshay@rtledgers.com>
2023-11-06 22:41:37 +0530

    11_react router dom setup completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+
 
 function App() {
   return (
-    <div>
-      <h1>Bllodd</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
