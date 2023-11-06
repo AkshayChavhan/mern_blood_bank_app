debf5818a9a83ffa573c2b62b2b9e48ccaa20a1f
akshaychavhan <akshay@rtledgers.com>
2023-11-06 10:34:01 +0530

    03 Packages middleware installed and used in app

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
 
 
+// dotenv setup
+const dotenv = require("dotenv");
+dotenv.config()
 // rest object
 const app = express()
 
-const PORT = 8081
+
+// middlewares
+app.use(express.json());  //to handle json response
+app.use(cors());          //handle cross origin port resources
+app.use(morgan("dev"))    // dev to get info on console for developer
+
+
+// port
+const PORT = process.env.PORT || 8081
 
 
 
@@ -13,7 +27,13 @@ const PORT = 8081
 app.use("/api/v1/", require("./routes/testRoutes") )
 
 
+
+
+
+
+
+
 // server listening
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port http://localhost:${PORT} in ${process.env.DEV_MODE}`)
 })
