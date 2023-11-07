42ad797a0a0148ef91ec528fcb515e8c76e75d44
akshaychavhan <akshay@rtledgers.com>
2023-11-07 22:43:24 +0530

    16_register api integrated using redux

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { userLogin } from "./authActions";
+import { userLogin , userRegister } from "./authActions";
 
-const token = localStorage.getItem("token") ? localStorage.getItem("token") : null ;
+const token = localStorage.getItem("token") ? localStorage.getItem("token") : null;
 
 const initialState = {
     loading: false,
@@ -12,22 +12,36 @@ const initialState = {
 
 const authSlice = createSlice({
     name: 'auth',
-    initialState: initialState ,
-    reducer:{},
-    extraReducers:(builder) => {
+    initialState: initialState,
+    reducer: {},
+    extraReducers: (builder) => {
+        // Login
         builder.addCase(userLogin.pending, (state) => {
             state.loading = true;
             state.error = null;
-          });
-          builder.addCase(userLogin.fulfilled, (state, { payload }) => {
+        });
+        builder.addCase(userLogin.fulfilled, (state, { payload }) => {
             state.loading = false;
             state.user = payload.user;
             state.token = payload.token;
-          });
-        builder.addCase(userLogin.rejected , (state , {payload})=>{
-            state.loading=false ;
+        });
+        builder.addCase(userLogin.rejected, (state, { payload }) => {
+            state.loading = false;
             state.error = payload;
-        })
+        });
+        // REGISTER user
+        builder.addCase(userRegister.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        });
+        builder.addCase(userRegister.fulfilled, (state, { payload }) => {
+            state.loading = false;
+            state.user = payload.user;
+        });
+        builder.addCase(userRegister.rejected, (state, { payload }) => {
+            state.loading = false;
+            state.error = payload;
+        });
 
     }
 })
