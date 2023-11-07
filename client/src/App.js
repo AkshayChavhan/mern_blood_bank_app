0e0065888644bfd4a4f66eb87e716f596e112e1a
akshaychavhan <akshay@rtledgers.com>
2023-11-07 16:37:11 +0530

    15_login action handled using redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import { ToastContainer } from 'react-toastify';
 
 
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,10 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { userLogin } from "./authActions";
 
+const token = localStorage.getItem("token") ? localStorage.getItem("token") : null ;
 
 const initialState = {
     loading: false,
     user: null,
-    token: null,
+    token: token,
     error: null
 }
 
@@ -12,7 +14,22 @@ const authSlice = createSlice({
     name: 'auth',
     initialState: initialState ,
     reducer:{},
-    extraReducers:{}
+    extraReducers:(builder) => {
+        builder.addCase(userLogin.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+          });
+          builder.addCase(userLogin.fulfilled, (state, { payload }) => {
+            state.loading = false;
+            state.user = payload.user;
+            state.token = payload.token;
+          });
+        builder.addCase(userLogin.rejected , (state , {payload})=>{
+            state.loading=false ;
+            state.error = payload;
+        })
+
+    }
 })
 
 
