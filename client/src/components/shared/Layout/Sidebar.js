b8341c3e5492250351765a6c9a3710ac5bd8c7c8
akshaychavhan <akshay@rtledgers.com>
2023-11-08 08:34:18 +0530

    19_navbar sidebar content area added

---
+++ client/src/components/shared/Layout/Sidebar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useLocation, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { userMenu } from './Menus/userMenu';
+import "../../../styles/Layout.css";
+
+
+const Sidebar = () => {
+
+  //GET USER STATE
+  const { user } = useSelector((state) => state.auth);
+
+  const location = useLocation();
+
+
+  return (
+    <div>
+      <div className="sidebar">
+        <div className="menu">
+          {
+            userMenu.map((menu) => {
+              const isActive = location.pathname === menu.path;
+
+              return (
+                <div className={`menu-item ${isActive && "active"}`}>
+                  <i className={menu.icon}></i>
+                  <Link to={menu.path}>{menu.name}</Link>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useSelector } from "react-redux";
+import Spinner from "../components/shared/Spinner";
+import { useNavigate } from 'react-router-dom';
+import Layout from '../components/shared/Layout/Layout';
 
 function Homepage() {
+  const navigate = useNavigate();
+  const { loading, error } = useSelector((state) => state.auth);
+
+  // if (user?.role === "admin") {
+  //   navigate("/admin");
+  // }
+
   return (
-    <div>Homepage</div>
+    <Layout>
+      {error && <span>{alert(error)}</span>}
+      {loading ? <Spinner /> :
+        <div className="container">
+          Homepage
+        </div>
+      }
+    </Layout>
   )
 }
 
-export default Homepage
+export default Homepage;
+
+
+
+
